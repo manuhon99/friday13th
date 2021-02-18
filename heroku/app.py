a805821ac9efc87e8b0648e5e7bd3896156e5f45
manuhon99 <manuhon99@gmail.com>
2021-02-18 17:07:23 -0300

    heroku

--- heroku/app.py
+++ heroku/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path='/static', static_folder='static')
+app = Flask(__name__, template_folder='templates', static_url_path='/static', static_folder='static')
 
 @app.route('/')
 def index():
